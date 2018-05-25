bc1e7eb6ccdfff481fa1627e36f93202ae641142
Nicholas Purington <32780379+NSPurington@users.noreply.github.com>
2018-05-24 23:45:48 -0700

    Changed database from sqlite to psql

--- lotsofalloys.py
+++ lotsofalloys.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
  
 from database_setup import BaseMetal, Base, Alloy, User
  
-engine = create_engine('sqlite:///metalcatalog.db')
+engine = create_engine('psql:///metalcatalog.db')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
